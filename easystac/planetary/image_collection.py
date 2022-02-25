d60cb6ee9c5bc81f73a84953d3512815c8d3fdd2
davemlz <dml.mont@gmail.com>
2022-02-25 14:29:53 +0100

    FEAT: Added ImageCollection for PC and radiant

---
+++ easystac/planetary/image_collection.py
@@ -0,0 +1,78 @@
+import json
+import os
+import warnings
+from pathlib import Path
+
+import stackstac
+import planetary_computer as pc
+
+from ..base import BaseImageCollection
+
+warnings.simplefilter("always", UserWarning)
+
+class ImageCollection(BaseImageCollection):
+    """ImageCollection object for Planetary Computer.
+
+    This object mimics the Earth Engine filtering methods for ee.ImageCollection class and
+    makes them available for the Planetary Computer STAC. Items signing is automatically
+    done.
+
+    Parameters
+    ----------
+    collection: str
+        Collection name.
+
+    Examples
+    --------
+    >>> import easystac.planetary as pc
+    >>> from geojson import Point
+    >>> pc.Authenticate()
+    >>> pc.Initialize()
+    >>> geom = Point([-76.1,4.3])
+    >>> S2 = (pc.ImageCollection("sentinel-2-l2a")
+            .filterBounds(geom)
+            .filterDate("2020-01-01","2021-01-01")
+            .getInfo(resolution = 10))
+    """
+
+    def getInfo(self, **kwargs):
+        """Returns all the information from the STAC search.
+
+        Parameters
+        ----------
+        **kwargs
+            Additional arguments passed to :code:`stackstac.stack()`. Some of them are
+            :code:`epsg`, :code:`resolution`, and :code:`bbox`.
+
+        Returns
+        -------
+        xarray.DataArray
+            Chunked DataArray with Dask.
+
+        Examples
+        --------
+        >>> import easystac.planetary as pc
+        >>> from geojson import Point
+        >>> pc.Authenticate()
+        >>> pc.Initialize()
+        >>> geom = Point([-76.1,4.3])
+        >>> S2 = (pc.ImageCollection("sentinel-2-l2a")
+                .filterBounds(geom)
+                .filterDate("2020-01-01","2021-01-01")
+                .getInfo(resolution = 10))
+        """
+        parameters = None
+
+        if "PC_SDK_SUBSCRIPTION_KEY" in os.environ:
+            parameters = {"subscription-key": os.environ["PC_SDK_SUBSCRIPTION_KEY"]}
+
+        search = self._search(
+            url="https://planetarycomputer.microsoft.com/api/stac/v1",
+            parameters=parameters,
+        )
+
+        items = [pc.sign(item).to_dict() for item in search.get_items()]
+
+        image_collection = stackstac.stack(items, **kwargs)
+
+        return image_collection
--- easystac/radiant/__init__.py
+++ easystac/radiant/__init__.py
@@ -5,8 +5,9 @@ from pathlib import Path
 
 import stackstac
 
-from ..base import BaseImageCollection
 from ..logging_utils import obtain_and_write_token
+
+from .image_collection import ImageCollection
 
 CREDENTIAL_FILE = "~/.config/easystac/credentials_radiant.json"
 
@@ -32,57 +33,3 @@ def Initialize():
         )
 
 
-class ImageCollection(BaseImageCollection):
-    """ImageCollection object for Radiant Earth ML Hub.
-
-    This object mimics the Earth Engine filtering methods for ee.ImageCollection class and
-    makes them available for the Radiant Earth ML Hub STAC.
-
-    Parameters
-    ----------
-    collection: str
-        Collection name.
-
-    Examples
-    --------
-    >>> import easystac.radiant as rd
-    >>> rd.Authenticate()
-    >>> rd.Initialize()
-    >>> S1floods = (rd.ImageCollection("sen12floods_s1_source")
-            .filterDate("2019-01-01","2019-01-05")
-            .getInfo(epsg = 4326,resolution = 0.0001))
-    """
-
-    def getInfo(self, **kwargs):
-        """Returns all the information from the STAC search.
-
-        Parameters
-        ----------
-        **kwargs
-            Additional arguments passed to :code:`stackstac.stack()`. Some of them are
-            :code:`epsg`, :code:`resolution`, and :code:`bbox`.
-
-        Returns
-        -------
-        xarray.DataArray
-            Chunked DataArray with Dask.
-
-        Examples
-        --------
-        >>> import easystac.radiant as rd
-        >>> rd.Authenticate()
-        >>> rd.Initialize()
-        >>> S1floods = (rd.ImageCollection("sen12floods_s1_source")
-                .filterDate("2019-01-01","2019-01-05")
-                .getInfo(epsg = 4326,resolution = 0.0001))
-        """
-        search = self._search(
-            url="https://api.radiant.earth/mlhub/v1/",
-            parameters={"key": os.environ["MLHUB_API_KEY"]},
-        )
-
-        items = [item.to_dict() for item in search.get_items()]
-
-        image_collection = stackstac.stack(items, **kwargs)
-
-        return image_collection

9e20aa265a2414c11a3d6bb86d34cfa2421b3d7b
davemlz <dml.mont@gmail.com>
2022-02-25 15:51:49 +0100

    TEST: Test Planetary Computer

---
+++ tests/test_easystac.py
@@ -0,0 +1,22 @@
+import xarray as xr
+from geojson import Point
+
+import easystac.planetary as pc
+
+
+class Test(unittest.TestCase):
+    """Tests for the easystac package."""
+
+    def test_planetary(self):
+        """Test the Planetary Computer extension"""
+        result = (
+            pc.ImageCollection("sentinel-2-l2a")
+            .filterBounds(geom)
+            .filterDate("2020-01-01", "2021-01-01")
+            .getInfo(resolution=10)
+        )
+        self.assertIsInstance(result, xr.DataArray)
+
+
+if __name__ == "__main__":
+    unittest.main()
